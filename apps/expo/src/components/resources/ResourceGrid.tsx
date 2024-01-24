64dd89017dfd2335644354b2e589c864a50b3cdc
brightonmboya <b.mboya@alustudent.com>
2024-01-24 13:46:07 +0200

    i am fucked

--- apps/expo/src/components/resources/ResourceGrid.tsx
+++ apps/expo/src/components/resources/ResourceGrid.tsx
@@ -1,22 +1,51 @@
+import { useRef } from "react";
 import { FlatList, View } from "react-native";
+import YoutubePlayer from "react-native-youtube-iframe";
+import { ResizeMode, Video } from "expo-av";
+
+
 
 import { Card, data } from "~/components/home/Resources";
 import H1 from "../ui/Heading";
+
+
+function VideoCard() {
+  const video = useRef(null);
+  return (
+    <View>
+      {/* <Video
+        ref={video}
+        className="h-[150px] w-[120px]"
+        source={{
+          uri: "https://www.youtube.com/embed/9Qt8ftivJ5A?modestbranding=1&playsinline=1&showinfo=0&rel=0",
+        }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping
+        // onPlaybackStatusUpdate={status => setStatus(() => status)}
+      /> */}
+     <YoutubePlayer
+        height={300}
+        play={true}
+        videoId={'84WIaK3bl_s'}
+      />
+    </View>
+  );
+}
 
 export default function ResourceGrid({ title }: { title: string }) {
   return (
     <View className="mt-5">
       {/* <P style="text-xl leading-[1.2em]" textType="heading">{title}</P> */}
-     <H1 styling="text-xl tracking-[0.32px]">{title}</H1>
+      <H1 styling="text-xl tracking-[0.32px]">{title}</H1>
       <FlatList
         data={data}
-        renderItem={({ item }) => <Card {...item} />}
+        renderItem={({ item }) => <VideoCard />}
         keyExtractor={(_, index) => index.toString()}
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{
-          // paddingHorizontal: 10,
           gap: 5,
-          marginTop: 10
+          marginTop: 10,
         }}
         horizontal={true}
       />
---
+++ apps/expo/src/components/resources/data.ts
@@ -0,0 +1,44 @@
+export const motivationalVideos = [
+  {
+    id: 1,
+    videoId: "ZebSXPUCPFc ",
+  },
+  {
+    id: 2,
+    videoId: "YuNLNeIorJw ",
+  },
+  {
+    id: 3,
+    videoId: "k25QB3D9Uss",
+  },
+];
+
+export const meditationVideos = [
+  {
+    id: 1,
+    videoId: "xv-ejEOogaA",
+  },
+  {
+    id: 2,
+    videoId: "ZVcQXX7fmFI",
+  },
+  {
+    id: 3,
+    videoId: "9Qt8ftivJ5A",
+  },
+];
+
+export const mentalRelaxationVideos = [
+  {
+    id: 1,
+    videoId: "DbDoBzGY3vo",
+  },
+  {
+    id: 2,
+    videoId: "ztTexqGQ0VI",
+  },
+  {
+    id: 3,
+    videoId: "HT_ZvD94_kE",
+  },
+]
