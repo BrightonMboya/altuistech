6c5f088276eb003bd1833b56b5d89afebd82e561
brightonmboya <b.mboya@alustudent.com>
2024-02-05 13:18:40 +0200

    edited the alcohol bottom sheet

--- apps/expo/src/components/home/bottom-sheets/alcohol-test.tsx
+++ apps/expo/src/components/home/bottom-sheets/alcohol-test.tsx
@@ -18,7 +18,7 @@ export default function Component({ bottomRef }: { bottomRef: any }) {
     // @ts-ignore
     <BottomSheet
       ref={bottomRef}
-      snapTo={"60%"}
+      snapTo={"40%"}
       backgroundColor={"white"}
       backDropColor={"black"}
     >
@@ -27,12 +27,12 @@ export default function Component({ bottomRef }: { bottomRef: any }) {
         className="pt-3"
       />
       <View className="flex flex-col items-center pt-5">
-        <H1 styling="text-lg text-[#121212] pl-3 w-[90%]">
+        <H1 styling="text-lg text-[#121212] pl-3 w-[90%] md:text-2xl">
           Do you take Alcohol Or Any Non-Prescribed Drug Substance?{" "}
         </H1>
 
         <View className="mt-5 flex w-[90%] flex-row items-start rounded-md bg-[#FFF2EB] p-4">
-          <Svg width="20" height="20" viewBox="0 0 16 16" fill="none">
+          <Svg width="30" height="30" viewBox="0 0 16 16" fill="none">
             <Path
               d="M8.00016 15.1667C4.04683 15.1667 0.833496 11.9533 0.833496 8C0.833496 4.04667 4.04683 0.833333 8.00016 0.833333C11.9535 0.833333 15.1668 4.04667 15.1668 8C15.1668 11.9533 11.9535 15.1667 8.00016 15.1667ZM8.00016 1.83333C4.60016 1.83333 1.8335 4.6 1.8335 8C1.8335 11.4 4.60016 14.1667 8.00016 14.1667C11.4002 14.1667 14.1668 11.4 14.1668 8C14.1668 4.6 11.4002 1.83333 8.00016 1.83333Z"
               fill="#FF2525"
@@ -46,33 +46,38 @@ export default function Component({ bottomRef }: { bottomRef: any }) {
               fill="#FF2525"
             />
           </Svg>
-          <P style="text-[#505050] pl-[10px]">
+          <P style="text-[#505050] pl-[10px] md:text-lg">
             You don’t need to take this test if you don’t take alcohol or
             Non-Prescribed drugs.
           </P>
         </View>
 
+        <View className="md:flex md:flex-row md:mt-10">
+
+
         <TouchableOpacity
-          className=" mt-[48px] h-12 w-[350px] rounded-md bg-[#1960F2]"
+          className=" mt-[48px] md:mt-0 h-12 w-[350px] rounded-md bg-[#1960F2]"
           onPress={() => {
             router.push("/tests/alcohol");
           }}
         >
-          <H1 styling="text-lg tracking-wide text-white pt-2 text-center">
+          <H1 styling="text-lg tracking-wide text-white pt-2 text-center md:text-xl">
             Yes I do
           </H1>
         </TouchableOpacity>
 
         <TouchableOpacity
-          className="mt-5 h-12 w-[350px] rounded-md border-[2px] border-[#1960F2]  bg-white "
+          className="mt-5 md:mt-0 h-12 w-[350px] rounded-md border-[2px] border-[#1960F2] md:ml-5  bg-white "
           onPress={() => {
             closeBottomSheet();
           }}
         >
-          <H1 styling="text-lg tracking-wide text-[#1960F2] pt-2 text-center">
+          <H1 styling="text-lg tracking-wide text-[#1960F2] pt-2 text-center  md:text-xl">
             No I don’t
           </H1>
         </TouchableOpacity>
+        </View>
+
       </View>
     </BottomSheet>
   );
